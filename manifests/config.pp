a0c78b75cb00aeb1417d1a5c323c5ccb0a68f3c2
Vitaliy <vnogin@mirantis.com>
2017-04-03 13:09:25 +0000

    Change config manifest
    Config manifest changed after init commit according to freezer
    project requirements.
    
    Change-Id: Ibbfbacf105babb6115861a88f415cca7d15bff76

--- manifests/config.pp
+++ manifests/config.pp
@@ -17,14 +17,26 @@
 #     DEFAULT/bar:
 #       value: barValue
 #
+# [*freezer_config*]
+#   (optional) Allow configuration of freezer.conf configurations.
+#   Defaults to empty hash'{}'
+#
+# [*api_paste_ini_config*]
+#   (optional) Allow configuration of /etc/freezer/freezer-paste.ini configurations.
+#
 #   NOTE: The configuration MUST NOT be already handled by this module
 #   or Puppet catalog compilation will fail with duplicate resources.
 #
 class freezer::config (
   $freezer_config = {},
+  $api_paste_ini_config  = {},
 ) {
 
+  include ::freezer::deps
+
   validate_hash($freezer_config)
+  validate_hash($api_paste_ini_config)
 
   create_resources('freezer_config', $freezer_config)
+  create_resources('freezer_api_paste_ini', $api_paste_ini_config)
 }
