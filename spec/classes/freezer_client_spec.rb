293aeeefcc0117cf0b9b250fbd6feef8f3cd925d
zhubingbing <zhu.bingbing@99cloud.net>
2018-04-12 10:57:09 +0800

    Debian is using python3-freezerclient
    Change-Id: I9bd0cbd5a6f09dc3233f11c24164c6a6e785b97b

--- spec/classes/freezer_client_spec.rb
+++ spec/classes/freezer_client_spec.rb
@@ -5,7 +5,7 @@ describe 'freezer::client' do
   shared_examples 'freezer client' do
     it { is_expected.to contain_class('freezer::params') }
     it { is_expected.to contain_package('python-freezerclient').with(
-        :name     => 'python-freezerclient',
+        :name     => platform_params[:client_package_name],
         :ensure   => 'present',
         :tag      => ['openstack', 'freezer-support-package'],
         :provider => 'pip',
@@ -19,6 +19,19 @@ describe 'freezer::client' do
     context "on #{os}" do
       let (:facts) do
         facts.merge!(OSDefaults.get_facts())
+      end
+
+      let(:platform_params) do
+        case facts[:osfamily]
+        when 'Debian'
+          if facts[:os_package_type] == 'debian'
+            { :client_package_name => 'python3-freezerclient' }
+          else
+            { :client_package_name => 'python-freezerclient' }
+          end
+        when 'RedHat'
+          { :client_package_name => 'python-freezerclient' }
+        end
       end
 
       it_configures 'freezer client'
