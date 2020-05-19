72718329e96a69ce9e87ad01ff2a16e2a5afeac7
Takashi Kajinami <tkajinam@redhat.com>
2020-05-19 22:48:57 +0900

    Remove password hash generation in each puppet modules
    ... and migrate it to openstacklib so that all logics about database
    configuration are implemented in one common place.
    
    Depends-on: https://review.opendev.org/#/c/728595/
    Change-Id: Ie66552bc2a4447abca9d685e461ac3837568583d

--- manifests/db/postgresql.pp
+++ manifests/db/postgresql.pp
@@ -35,11 +35,11 @@ class freezer::db::postgresql(
   include freezer::deps
 
   ::openstacklib::db::postgresql { 'freezer':
-    password_hash => postgresql_password($user, $password),
-    dbname        => $dbname,
-    user          => $user,
-    encoding      => $encoding,
-    privileges    => $privileges,
+    password   => $password,
+    dbname     => $dbname,
+    user       => $user,
+    encoding   => $encoding,
+    privileges => $privileges,
   }
 
   Anchor['freezer::db::begin']
